b4131903c7cc553524b527d39d366db9204e5eac
BL-GS <13413419347@163.com>
2023-10-21 20:35:06 +0800

    first commit

---
+++ include/thread_allocator/abstract_thread_allocator.h
@@ -0,0 +1,41 @@
+/*
+ * @author: BL-GS 
+ * @date:   2023/3/11
+ */
+
+#pragma once
+#ifndef THREAD_ALLOC_ABSTRACT_THREAD_ALLOCATOR_H
+#define THREAD_ALLOC_ABSTRACT_THREAD_ALLOCATOR_H
+
+#include <chrono>
+#include <concepts>
+#include <coroutine>
+#include <functional>
+
+namespace allocator {
+
+	enum class ThreadBindStrategy {
+		None,
+		NUMABind,
+		RemoteNUMABind,
+		CPUBind,
+		HybridBind
+	};
+
+	template<class Allocator>
+	concept ThreadAllocatorConcept = requires(Allocator allocator, int thread_num) {
+
+		{ allocator.reserve(thread_num) };
+
+		{ allocator.wait_all_tasks() };
+
+		{ allocator.clear_all_tasks() };
+
+		/// Task execution with self-control mechanism required
+		{ allocator.run_tasks(static_cast<int>(0), [](){}) } -> std::convertible_to<uint64_t>;
+
+	};
+
+}
+
+#endif //THREAD_ALLOC_ABSTRACT_THREAD_ALLOCATOR_H
